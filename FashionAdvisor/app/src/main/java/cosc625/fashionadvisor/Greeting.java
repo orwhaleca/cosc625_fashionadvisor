ee9faa45d56e657e56f25008a7c44cb01270347b
Matthew Benson <blitzkriegmlg@gmail.com>
2017-02-15 22:39:07 -0500

    Added Android Project with one empty activity.

---
+++ FashionAdvisor/app/src/main/java/cosc625/fashionadvisor/Greeting.java
@@ -0,0 +1,13 @@
+package cosc625.fashionadvisor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Greeting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_greeting);
+    }
+}
