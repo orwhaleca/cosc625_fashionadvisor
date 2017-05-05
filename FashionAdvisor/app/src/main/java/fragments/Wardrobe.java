64161655498aa79770d432fc6c274db46a402a67
Matthew Benson <Matt@MacBook.local>
2017-05-05 16:25:34 -0400

    Added a floating button to the wardrobe layout which calls the method addPhotoClick in Wardrobe.

--- FashionAdvisor/app/src/main/java/fragments/Wardrobe.java
+++ FashionAdvisor/app/src/main/java/fragments/Wardrobe.java
@@ -1,10 +1,13 @@
 package fragments;
 
 import android.os.Bundle;
+import android.support.annotation.StringRes;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import cosc625.fashionadvisor.R;
 
@@ -14,6 +17,9 @@ import cosc625.fashionadvisor.R;
 
 //TODO: Allow users to modify / add clothing here
 public class Wardrobe extends Fragment {
+
+    TextView textView;
+    FloatingActionButton button;
 
     public Wardrobe() {
         //empty constructor
@@ -29,6 +35,24 @@ public class Wardrobe extends Fragment {
                              ViewGroup container,
                              Bundle savedInstanceState) {
         //Inflate the layout for this fragment
-        return inflater.inflate(R.layout.wardrobe, container, false);
+        View view = inflater.inflate(R.layout.wardrobe, container, false);
+        textView = (TextView) view.findViewById(R.id.myText);
+        button = (FloatingActionButton) view.findViewById(R.id.addClothingButton);
+
+        //we could use the xml solution of android:onClick, but then our button methods
+        //need to be defined in MainTabbed rather than the fragment classes
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addPhotoClick(v);
+            }
+        });
+
+        return view;
+    }
+
+    public void addPhotoClick(View view) {
+        System.out.println("Button pressed");
+        textView.setText("You've indicated that you would like to take a picture for the wardrobe.");
     }
 }
