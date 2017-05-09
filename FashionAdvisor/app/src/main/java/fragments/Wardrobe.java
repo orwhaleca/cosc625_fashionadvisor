650f9e9b16504898b6b2c5deb394baa88393deaf
Matthew Benson <Matt@MacBook.local>
2017-05-09 15:29:12 -0400

    Added an imageView to wardrobe.xml which will get the result of the camera intent

--- FashionAdvisor/app/src/main/java/fragments/Wardrobe.java
+++ FashionAdvisor/app/src/main/java/fragments/Wardrobe.java
@@ -2,6 +2,7 @@ package fragments;
 
 import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.StringRes;
@@ -12,10 +13,13 @@ import android.support.v4.content.PermissionChecker;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import cosc625.fashionadvisor.PermissionHandler;
 import cosc625.fashionadvisor.R;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by Matt on 5/4/17.
@@ -25,7 +29,10 @@ import cosc625.fashionadvisor.R;
 public class Wardrobe extends Fragment {
 
     TextView textView;
+    ImageView imageView;
     FloatingActionButton button;
+
+    private final int REQUEST_IMAGE_CAPTURE = 1;
 
     public Wardrobe() {
         //empty constructor
@@ -43,6 +50,7 @@ public class Wardrobe extends Fragment {
         //Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.wardrobe, container, false);
         textView = (TextView) view.findViewById(R.id.myText);
+        imageView = (ImageView) view.findViewById(R.id.imageView);
         button = (FloatingActionButton) view.findViewById(R.id.addClothingButton);
 
         //we could use the xml solution of android:onClick, but then our button methods
@@ -61,22 +69,18 @@ public class Wardrobe extends Fragment {
         System.out.println("Button pressed");
         textView.setText("You've indicated that you would like to take a picture for the wardrobe.");
 
-        /* This was all pointless?
-        //not certain that getActivity is the best option here
-        PermissionHandler.checkPermission(getActivity(), Manifest.permission.CAMERA);
-
-        //I think we let the permission handler call us back to call the method which changes
-        //the intent...
-
-
-        //need to check permission for camera before executing
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        startActivity(intent);
-        */
-
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(view.getContext().getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, 1);//change this 1 to class constant
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
         }
     }
 }
